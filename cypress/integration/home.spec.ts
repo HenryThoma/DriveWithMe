ca17d29f064da19652901817115998e0e7e45e23
Henry <bluemlein4@web.de>
2019-11-20 22:25:32 +0100

    Adding more Tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,9 +1,33 @@
 context('home', () => {
-    it('adding Carpool', () => {
+    const checkAddDialogExists = () => {
+        cy.get('[cy-data=saveBtn]').click();
+        cy.get('[cy-data=AddCarpoolDlg]').should('exist');
+        cy.get('.alert-button').click();
+    };
+    const visitHome = () => {
         cy.visit('/mitfahrzentrale');
         cy.url().should('include', '/mitfahrzentrale');
+    };
+    const openAddDlg = () => {
+        visitHome();
         cy.get('[cy-data=addCarpool]').click();
         cy.get('[cy-data=AddCarpoolDlg]').should('exist');
-    });
+    };
 
+    it('open AddCarpool', () => {
+        openAddDlg();
+    });
+    it('check required Fields', () => {
+        openAddDlg();
+        // Try Saving without insert
+        checkAddDialogExists();
+        // Fill Name and try Save
+        cy.get('.carpoolDrive').type('TestFahrer').should('have.value', 'TestFahrer');
+        checkAddDialogExists();
+        cy.get('.carpoolDrive > .native-input').clear();
+        // Cant set ComboBox must fix later...
+        cy.get('.ng-pristine > .native-input').clear().type('5').should('have.value', '5');
+        checkAddDialogExists();
+    });
 });
+
