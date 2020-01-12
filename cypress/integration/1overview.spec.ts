11535c3a85b5477c54891ddccb4ec7cca8c5c517
Henry <bluemlein4@web.de>
2020-01-12 20:02:10 +0100

    Testing Overview Test in FullHD and mobile

--- cypress/integration/1overview.spec.ts
+++ cypress/integration/1overview.spec.ts
@@ -1,12 +1,36 @@
-context('overview', () => {
-    it('visit Home', () => {
-        cy.visit('');
-        cy.get('[cy-data=home]').click();
-        cy.url().should('include', '/mitfahrzentrale');
+import {openSidebar} from '../support/overview.po';
+
+describe('overview', () => {
+    context('1080p resolution', () => {
+        beforeEach(() => {
+            cy.viewport(1920, 1080);
+        });
+        it('visit Home', () => {
+            cy.visit('');
+            cy.get('[cy-data=home]').click();
+            cy.url().should('include', '/mitfahrzentrale');
+        });
+        it('visit Export', () => {
+            cy.visit('');
+            cy.get('[cy-data=export]').click();
+            cy.url().should('include', '/export');
+        });
     });
-    it('visit Export', () => {
-        cy.visit('');
-        cy.get('[cy-data=export]').click();
-        cy.url().should('include', '/export');
+    context('iphone-x', () => {
+        beforeEach(() => {
+            cy.viewport('iphone-x');
+        });
+        it('visit Home', () => {
+            cy.visit('');
+            openSidebar();
+            cy.get('[cy-data=home]').click();
+            cy.url().should('include', '/mitfahrzentrale');
+        });
+        it('visit Export', () => {
+            cy.visit('');
+            openSidebar();
+            cy.get('[cy-data=export]').click();
+            cy.url().should('include', '/export');
+        });
     });
 });
