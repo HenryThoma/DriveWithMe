00c4bf6e2fc2dcac14876020fcdc9d72978ea795
Henry <bluemlein4@web.de>
2020-05-21 19:55:26 +0200

    Delete dublicate Code

--- cypress/integration/3-detail-page/carpool.spec.ts
+++ cypress/integration/3-detail-page/carpool.spec.ts
@@ -1,20 +1,4 @@
-const visitHome = () => {
-    cy.visit('/mitfahrzentrale');
-    cy.url().should('include', '/mitfahrzentrale');
-};
-const openCarpoolPage = () => {
-    visitHome();
-    cy.get('[cy-data=carpool0]').click();
-    cy.url().should('include', 'carpool');
-};
-const resetCosts = () => {
-    openCarpoolPage();
-    cy.wait(500);
-    cy.get('[cy-data=billRide]').click();
-    cy.get('[cy-data=payRide]').click();
-    cy.get('billingDialog').should('not.exist');
-    cy.get('.price').should('have.text','Aktuell belaufen sich deine Spritkosten auf 0€');
-};
+import { openCarpoolPage, resetCosts } from "../helpfer.func";
 
 describe('carpool', () => {
     it('open Carpoolpage', () => {
